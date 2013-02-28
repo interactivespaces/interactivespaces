7e3620f81fe2824d22723339271d1b10bf512e52
Keith Hughes <khughes@google.com>
2013-02-28 15:33:39 -0700

    Add clean and harsh shutdown of IS in BIN folder.

---
+++ interactivespaces_build/template-common/bin/harsh_shutdown_linux.bash
@@ -0,0 +1,14 @@
+# Figure out, regardless of any symlinks, aliases, etc, where this script
+# is located.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Get to the directory above the bin directory.
+cd $DIR/..
+
+# Shutdown Interactive Spaces very harshly
+kill -9 `cat run/interactivespaces.pid`
+rm run/interactivespaces.pid
+
+
---
+++ interactivespaces_build/template-common/bin/shutdown_linux.bash
@@ -0,0 +1,13 @@
+# Figure out, regardless of any symlinks, aliases, etc, where this script
+# is located.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Get to the directory above the bin directory.
+cd $DIR/..
+
+# Shutdown Interactive Spaces nicely
+mkdir -p run/control
+touch run/control/shutdown
+
