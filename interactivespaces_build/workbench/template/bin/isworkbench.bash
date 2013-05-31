a6fd81f2d55492aa66dd206fb6d4c07ab028d3b0
Keith Hughes <khughes@google.com>
2013-05-31 17:28:15 -0600

    Bash script for running workbench.

---
+++ interactivespaces_build/workbench/template/bin/isworkbench.bash
@@ -0,0 +1,11 @@
+# Figure out, regardless of any symlinks, aliases, etc, where this script
+# is located.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Get to the directory above the bin directory.
+cd $DIR/..
+
+java -server -jar interactivespaces-launcher-${interactivespaces.version}.jar ${@}
+
