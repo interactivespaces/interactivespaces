1adc4f9d64dcb056617a6a3fd59236eb87130548
Trevor Pering <peringknife@google.com>
2014-07-09 06:24:58 -0700

    Cleanup

--- interactivespaces-core/src/main/java/interactivespaces/util/TestRunnerBridge.java
+++ interactivespaces-core/src/main/java/interactivespaces/util/TestRunnerBridge.java
@@ -17,14 +17,12 @@ import java.util.List;
  *
  * @author Trevor Pering
  */
-public final class TestRunnerBridge {
+public class TestRunnerBridge extends RunListener {
 
-  public static final String RUNNER_METHOD_NAME = "runTests";
   /**
-   * Private constructor for utility class.
+   * Method name to use for running tests. This is used for reflection, but can't (easily) be specified in code.
    */
-  private TestRunnerBridge() {
-  }
+  public static final String RUNNER_METHOD_NAME = "runTests";
 
   /**
    * Run the given tests in the given classLoader.
@@ -38,61 +36,7 @@ public final class TestRunnerBridge {
    */
   public static boolean runTests(List<String> testClassNames, URLClassLoader classLoader) {
     JUnitCore junit = new JUnitCore();
-    junit.addListener(new RunListener() {
-
-      @Override
-      public void testRunStarted(Description description) throws Exception {
-        System.out.println("Starting JUnit tests");
-      }
-
-      @Override
-      public void testRunFinished(Result result) throws Exception {
-        System.out.println("Finished JUnit tests.");
-      }
-
-      @Override
-      public void testStarted(Description description) throws Exception {
-        System.out.format("Starting test %s \n", description.getDisplayName());
-      }
-
-      @Override
-      public void testFailure(Failure failure) throws Exception {
-        System.out.format("Test %s failed\n%s\n", failure.getTestHeader(), trimStackTrace(failure));
-      }
-
-      @Override
-      public void testAssumptionFailure(Failure failure) {
-        System.out.format("Test %s failed\n%s\n", failure.getTestHeader(), trimStackTrace(failure));
-      }
-
-      @Override
-      public void testFinished(Description description) throws Exception {
-        System.out.format("Finished test %s \n", description.getDisplayName());
-      }
-
-      @Override
-      public void testIgnored(Description description) throws Exception {
-        System.out.format("Ignoring test %s\n", description.getDisplayName());
-      }
-
-      /**
-       * Trim stack trace to stop before everything about the fact that Java
-       * reflection called the test method.
-       *
-       * @param failure
-       *          the JUnit failure
-       *
-       * @return the trimmed stack trace
-       */
-      private String trimStackTrace(Failure failure) {
-        String trace = failure.getTrace();
-        String testclass = failure.getDescription().getClassName();
-        int lastIndexOf = trace.lastIndexOf(testclass);
-        int endOfLine = trace.indexOf("\n", lastIndexOf);
-
-        return trace.substring(0, endOfLine);
-      }
-    });
+    junit.addListener(new TestRunnerBridge());
 
     boolean allSuceeded = true;
     for (String testClass : testClassNames) {
@@ -112,4 +56,56 @@ public final class TestRunnerBridge {
     return allSuceeded;
   }
 
+  @Override
+  public void testRunStarted(Description description) throws Exception {
+    System.out.println("Starting JUnit tests");
+  }
+
+  @Override
+  public void testRunFinished(Result result) throws Exception {
+    System.out.println("Finished JUnit tests.");
+  }
+
+  @Override
+  public void testStarted(Description description) throws Exception {
+    System.out.format("Starting test %s \n", description.getDisplayName());
+  }
+
+  @Override
+  public void testFailure(Failure failure) throws Exception {
+    System.out.format("Test %s failed\n%s\n", failure.getTestHeader(), trimStackTrace(failure));
+  }
+
+  @Override
+  public void testAssumptionFailure(Failure failure) {
+    System.out.format("Test %s failed\n%s\n", failure.getTestHeader(), trimStackTrace(failure));
+  }
+
+  @Override
+  public void testFinished(Description description) throws Exception {
+    System.out.format("Finished test %s \n", description.getDisplayName());
+  }
+
+  @Override
+  public void testIgnored(Description description) throws Exception {
+    System.out.format("Ignoring test %s\n", description.getDisplayName());
+  }
+
+  /**
+   * Trim stack trace to stop before everything about the fact that Java
+   * reflection called the test method.
+   *
+   * @param failure
+   *          the JUnit failure
+   *
+   * @return the trimmed stack trace
+   */
+  private String trimStackTrace(Failure failure) {
+    String trace = failure.getTrace();
+    String testclass = failure.getDescription().getClassName();
+    int lastIndexOf = trace.lastIndexOf(testclass);
+    int endOfLine = trace.indexOf("\n", lastIndexOf);
+
+    return trace.substring(0, endOfLine);
+  }
 }
