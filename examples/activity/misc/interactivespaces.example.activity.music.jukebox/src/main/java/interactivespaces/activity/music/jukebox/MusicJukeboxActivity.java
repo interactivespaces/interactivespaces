52e5d28f264677abe0204aa7fa0f50e0a565fa81
Keith Hughes <khughes@google.com>
2014-09-03 12:20:26 -0600

    1.6.3 final release commit. Fix jukebox example.
    [Review issue ]

--- examples/activity/misc/interactivespaces.example.activity.music.jukebox/src/main/java/interactivespaces/activity/music/jukebox/MusicJukeboxActivity.java
+++ examples/activity/misc/interactivespaces.example.activity.music.jukebox/src/main/java/interactivespaces/activity/music/jukebox/MusicJukeboxActivity.java
@@ -16,26 +16,29 @@
 
 package interactivespaces.activity.music.jukebox;
 
+import interactivespaces.SimpleInteractiveSpacesException;
 import interactivespaces.activity.impl.ros.BaseRosActivity;
 import interactivespaces.configuration.Configuration;
-import interactivespaces.service.audio.player.AudioRepository;
 import interactivespaces.service.audio.player.AudioTrack;
+import interactivespaces.service.audio.player.AudioTrackPlayer;
 import interactivespaces.service.audio.player.PlayableAudioTrack;
-import interactivespaces.service.audio.player.internal.JLayerAudioTrackPlayerFactory;
 import interactivespaces.service.audio.player.internal.ScanningFileAudioRepository;
+import interactivespaces.service.audio.player.internal.jlayer.JLayerAudioTrackPlayer;
 import interactivespaces.service.audio.player.jukebox.AudioJukebox;
 import interactivespaces.service.audio.player.jukebox.AudioJukeboxListener;
-import interactivespaces.service.audio.player.jukebox.BasicAudioJukebox;
-import interactivespaces.service.audio.player.jukebox.JukeboxOperation;
+import interactivespaces.service.audio.player.jukebox.internal.simple.SimpleAudioJukebox;
 import interactivespaces.util.ros.RosPublishers;
 import interactivespaces.util.ros.RosSubscribers;
 
-import org.ros.message.MessageFactory;
-
 import interactivespaces_msgs.MusicJukeboxAnnounce;
 import interactivespaces_msgs.MusicJukeboxControl;
+import org.python.google.common.collect.Lists;
+import org.ros.message.MessageFactory;
 import org.ros.message.MessageListener;
 import org.ros.node.ConnectedNode;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * The Music Jukebox activity.
@@ -44,16 +47,27 @@ import org.ros.node.ConnectedNode;
  */
 public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukeboxListener {
 
+  /**
+   * The configuration parameter name for the base folders for obtaining music.
+   */
+  public static final String CONFIGURATION_MUSIC_JUKEBOX_MUSIC_REPOSITORY_BASE = "space.music.repository.base";
+
+  /**
+   * ROS topic name for jukebox control.
+   */
   public static final String CONFIGURATION_MUSIC_JUKEBOX_CONTROL_ROS_TOPIC_NAME =
       "music.jukebox.control.ros.topic.name";
 
+  /**
+   * ROS topic name for announcing what is currently playing.
+   */
   public static final String CONFIGURATION_MUSIC_JUKEBOX_ANNOUNCE_ROS_TOPIC_NAME =
       "music.jukebox.announce.ros.topic.name";
 
   /**
    * The music repository this is a jukebox for.
    */
-  private AudioRepository musicRepository;
+  private ScanningFileAudioRepository musicRepository;
 
   /**
    * ROS subscribers for the jukebox control messages.
@@ -79,37 +93,24 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
   public void onActivityStartup() {
     getLog().info("Music jukebox starting!");
 
-    try {
-      Configuration configuration = getConfiguration();
+    Configuration configuration = getConfiguration();
 
-      setupRosTopics(configuration);
-      startMusicRepository(configuration);
+    setupRosTopics(configuration);
+    startMusicRepository(configuration);
 
-      // TODO(keith): Get from a service repository.
-      JLayerAudioTrackPlayerFactory trackPlayerFactory = new JLayerAudioTrackPlayerFactory();
-      trackPlayerFactory.setSpaceEnvironment(getSpaceEnvironment());
+    AudioTrackPlayer trackPlayer = new JLayerAudioTrackPlayer(getSpaceEnvironment().getExecutorService(), getLog());
+    addManagedResource(trackPlayer);
 
-      jukebox =
-          new BasicAudioJukebox(musicRepository, trackPlayerFactory, getSpaceEnvironment()
-              .getExecutorService(), configuration, getLog());
-      jukebox.setListener(this);
+    jukebox = new SimpleAudioJukebox(musicRepository, trackPlayer, getLog());
+    jukebox.addListener(this);
 
-      jukebox.startup();
+    addManagedResource(jukebox);
 
-      getLog().info("Music jukebox ready to spin the tunes!");
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    getLog().info("Music jukebox ready to spin the tunes!");
   }
 
   @Override
   public void onActivityCleanup() {
-    if (jukebox != null) {
-      jukebox.shutdown();
-      jukebox = null;
-    }
-
     if (jukeboxAnnouncePublishers != null) {
       jukeboxAnnouncePublishers.shutdown();
       jukeboxAnnouncePublishers = null;
@@ -118,11 +119,6 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
     if (jukeboxControlSubscribers != null) {
       jukeboxControlSubscribers.shutdown();
       jukeboxControlSubscribers = null;
-    }
-
-    if (musicRepository != null) {
-      musicRepository.shutdown();
-      musicRepository = null;
     }
   }
 
@@ -147,30 +143,39 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
     rosMessageFactory = node.getTopicMessageFactory();
 
     jukeboxControlSubscribers = new RosSubscribers<MusicJukeboxControl>(getLog());
-    jukeboxControlSubscribers
-        .addSubscribers(node, "interactivespaces_msgs/MusicJukeboxControl", configuration
-            .getRequiredPropertyString(CONFIGURATION_MUSIC_JUKEBOX_CONTROL_ROS_TOPIC_NAME),
-            new MessageListener<MusicJukeboxControl>() {
-              @Override
-              public void onNewMessage(MusicJukeboxControl request) {
-                handleNewJukeboxCommand(request);
-              }
-            });
+    jukeboxControlSubscribers.addSubscribers(node, "interactivespaces_msgs/MusicJukeboxControl",
+        configuration.getRequiredPropertyString(CONFIGURATION_MUSIC_JUKEBOX_CONTROL_ROS_TOPIC_NAME),
+        new MessageListener<MusicJukeboxControl>() {
+          @Override
+          public void onNewMessage(MusicJukeboxControl request) {
+            handleNewJukeboxCommand(request);
+          }
+        });
 
     jukeboxAnnouncePublishers = new RosPublishers<MusicJukeboxAnnounce>(getLog());
     jukeboxAnnouncePublishers.addPublishers(node, "interactivespaces_msgs/MusicJukeboxAnnounce",
-        configuration
-            .getRequiredPropertyString(CONFIGURATION_MUSIC_JUKEBOX_ANNOUNCE_ROS_TOPIC_NAME));
+        configuration.getRequiredPropertyString(CONFIGURATION_MUSIC_JUKEBOX_ANNOUNCE_ROS_TOPIC_NAME));
   }
 
   /**
    * Start up the music repository.
    */
   private void startMusicRepository(Configuration configuration) {
-    musicRepository = new ScanningFileAudioRepository();
+    List<String> baseRepositoryFiles =
+        configuration.getPropertyStringList(CONFIGURATION_MUSIC_JUKEBOX_MUSIC_REPOSITORY_BASE, ":");
+    if (baseRepositoryFiles != null) {
+      List<File> baseRespositories = Lists.newArrayList();
+      for (String baseRepositoryFile : baseRepositoryFiles) {
 
-    musicRepository.setConfiguration(configuration);
-    musicRepository.startup();
+      }
+      musicRepository = new ScanningFileAudioRepository();
+
+      musicRepository.setRepositoryBases(baseRespositories);
+      addManagedResource(musicRepository);
+    } else {
+      throw new SimpleInteractiveSpacesException(String.format("No configuration parameter %s",
+          CONFIGURATION_MUSIC_JUKEBOX_MUSIC_REPOSITORY_BASE));
+    }
   }
 
   /**
@@ -184,8 +189,7 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
       // TODO(keith): Add track queueing operation
       switch (request.getOperation()) {
         case MusicJukeboxControl.OPERATION_PLAY_TRACK:
-          jukebox.startPlayTrackOperation(request.getId(), request.getBegin(),
-              request.getDuration());
+          jukebox.startPlayTrackOperation(request.getId());
 
           break;
 
@@ -206,13 +210,12 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
           getLog().error(String.format("Unknown music jukebox command %d", request.getOperation()));
       }
     } catch (Exception e) {
-      getLog().error(
-          String.format("Error during music jukebox command %d", request.getOperation()), e);
+      getLog().error(String.format("Error during music jukebox command %d", request.getOperation()), e);
     }
   }
 
   @Override
-  public void onJukeboxTrackStart(JukeboxOperation operation, PlayableAudioTrack ptrack) {
+  public void onJukeboxTrackStart(AudioJukebox jukebox, PlayableAudioTrack ptrack) {
     if (getLog().isInfoEnabled()) {
       getLog().info(String.format("Playing %s", ptrack));
     }
@@ -227,7 +230,7 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
   }
 
   @Override
-  public void onJukeboxTrackStop(JukeboxOperation operation, PlayableAudioTrack ptrack) {
+  public void onJukeboxTrackStop(AudioJukebox jukebox, PlayableAudioTrack ptrack) {
     // Everyone gets told we have completed the track.
     if (getLog().isInfoEnabled()) {
       getLog().info(String.format("Done playing %s", ptrack));
@@ -238,7 +241,7 @@ public class MusicJukeboxActivity extends BaseRosActivity implements AudioJukebo
   }
 
   @Override
-  public void onJukeboxOperationComplete(JukeboxOperation operation) {
+  public void onJukeboxOperationComplete(AudioJukebox jukebox) {
     getLog().info("Operation completed");
   }
 
