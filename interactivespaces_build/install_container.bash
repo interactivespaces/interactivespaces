202c665a44262333ca8a6482936f65991f10eb6d
Max Rebuschatis <lincolnfrog@google.com>
2015-06-02 11:45:02 -0700

    Fix for installDev script so that it creates all necessary directories. Also added quotes around paths as per Google style guide.

--- interactivespaces_build/install_container.bash
+++ interactivespaces_build/install_container.bash
@@ -12,54 +12,59 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-VERSION=$1
-CONTAINER_TYPE=$2
-CONTAINER=$3
-STAGING=$4
+VERSION="$1"
+CONTAINER_TYPE="$2"
+CONTAINER="$3"
+STAGING="$4"
 
 # How to execute a command
 DO_CMD=
 #DO_CMD=echo
 
-echo Installing IS version ${VERSION} into container ${CONTAINER} of type ${CONTAINER_TYPE} from ${STAGING}
+echo Installing IS version "${VERSION}" into container "${CONTAINER}" of type "${CONTAINER_TYPE}" from "${STAGING}"
 
-CONTAINER_BIN=${CONTAINER}/bin
-CONTAINER_BOOTSTRAP=${CONTAINER}/bootstrap
-CONTAINER_CONFIG=${CONTAINER}/config
-CONTAINER_LIB_SYSTEM_JAVA=${CONTAINER}/lib/system/java
-CONTAINER_EXTRAS=${CONTAINER}/extras
-CONTAINER_TEMPLATES=${CONTAINER}/templates
+CONTAINER_BIN="${CONTAINER}/bin"
+CONTAINER_BOOTSTRAP="${CONTAINER}/bootstrap"
+CONTAINER_CONFIG="${CONTAINER}/config"
+CONTAINER_LIB_SYSTEM_JAVA="${CONTAINER}/lib/system/java"
+CONTAINER_EXTRAS="${CONTAINER}/extras"
+CONTAINER_TEMPLATES="${CONTAINER}/templates"
 
-${DO_CMD} rm  ${CONTAINER_BOOTSTRAP}/*
-${DO_CMD} rm  ${CONTAINER_LIB_SYSTEM_JAVA}/*.jar
-${DO_CMD} rm  ${CONTAINER}/interactivespaces-launcher-*.jar
+${DO_CMD} rm -f "${CONTAINER_BOOTSTRAP}/"*
+${DO_CMD} rm -f "${CONTAINER_LIB_SYSTEM_JAVA}/"*.jar
+${DO_CMD} rm -f "${CONTAINER}/interactivespaces-launcher-"*.jar
 
-if [ ${CONTAINER_TYPE} == "controller" ]; then
-  ${DO_CMD} rm -fR ${CONTAINER_EXTRAS}/*
+if [ "${CONTAINER_TYPE}" == "controller" ]; then
+  ${DO_CMD} rm -rf "${CONTAINER_EXTRAS}/"*
 fi
 
-if [ ${CONTAINER_TYPE} == "workbench" ]; then
-  ${DO_CMD} rm -fR ${CONTAINER_TEMPLATES}/*
+if [ "${CONTAINER_TYPE}" == "workbench" ]; then
+  ${DO_CMD} rm -rf "${CONTAINER_TEMPLATES}/"*
 fi
 
-${DO_CMD} mkdir -p ${CONTAINER_CONFIG}/system
+${DO_CMD} mkdir -p "${CONTAINER_CONFIG}/system"
+${DO_CMD} mkdir -p "${CONTAINER_BOOTSTRAP}"
+${DO_CMD} mkdir -p "${CONTAINER_BIN}"
+${DO_CMD} mkdir -p "${CONTAINER_LIB_SYSTEM_JAVA}"
+${DO_CMD} mkdir -p "${CONTAINER}"
+${DO_CMD} mkdir -p "${CONTAINER_EXTRAS}"
+${DO_CMD} mkdir -p "${CONTAINER_TEMPLATES}"
 
-${DO_CMD} cp ${STAGING}/config/system/* ${CONTAINER_CONFIG}/system
-${DO_CMD} cp ${STAGING}/bootstrap/* ${CONTAINER_BOOTSTRAP}
-${DO_CMD} cp ${STAGING}/bin/* ${CONTAINER_BIN}
-${DO_CMD} cp ${STAGING}/lib/system/java/delegations.conf ${CONTAINER_LIB_SYSTEM_JAVA}
-${DO_CMD} cp ${STAGING}/lib/system/java/*.jar ${CONTAINER_LIB_SYSTEM_JAVA}
-${DO_CMD} cp ${STAGING}/interactivespaces-launcher-${VERSION}.jar ${CONTAINER}
+${DO_CMD} cp "${STAGING}/config/system/"* "${CONTAINER_CONFIG}/system"
+${DO_CMD} cp "${STAGING}/bootstrap/"* "${CONTAINER_BOOTSTRAP}"
+${DO_CMD} cp "${STAGING}/bin/"* "${CONTAINER_BIN}"
+${DO_CMD} cp "${STAGING}/lib/system/java/delegations.conf" "${CONTAINER_LIB_SYSTEM_JAVA}"
+${DO_CMD} cp "${STAGING}/lib/system/java/"*.jar "${CONTAINER_LIB_SYSTEM_JAVA}"
+${DO_CMD} cp "${STAGING}/interactivespaces-launcher-${VERSION}.jar" "${CONTAINER}"
 
-
-if [ ${CONTAINER_TYPE} == "controller" ]; then
-  ${DO_CMD} cp -R ${STAGING}/extras/* ${CONTAINER_EXTRAS}
+if [ "${CONTAINER_TYPE}" == "controller" ]; then
+  ${DO_CMD} cp -R "${STAGING}/extras/"* "${CONTAINER_EXTRAS}"
 fi
 
-if [ ${CONTAINER_TYPE} == "workbench" ]; then
-  ${DO_CMD} cp -R ${STAGING}/templates/* ${CONTAINER_TEMPLATES}
-  mkdir -p ${CONTAINER_EXTRAS}
-  ${DO_CMD} cp -R ${STAGING}/extras/* ${CONTAINER_EXTRAS}
+if [ "${CONTAINER_TYPE}" == "workbench" ]; then
+  ${DO_CMD} cp -R "${STAGING}/templates/"* "${CONTAINER_TEMPLATES}"
+  mkdir -p "${CONTAINER_EXTRAS}"
+  ${DO_CMD} cp -R "${STAGING}/extras/"* "${CONTAINER_EXTRAS}"
 fi
 
 
